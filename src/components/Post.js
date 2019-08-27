e494c851ea405f7547b816d0e44cd6b72ea094dc
Helton Isac <helton.isac@gmail.com>
2019-08-27 09:57:28 -0300

    Adding like button

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,24 +5,56 @@ import {
     Text,
     Image,
     Dimensions,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 const screenWidth = Dimensions.get('screen').width;
 
+
 export default class Post extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: this.props.foto
+        };
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? require('../../resources/img/s2-checked.png')
+            : require('../../resources/img/s2.png')
+    }
+
+    like() {
+        const fotoAtualizada = {
+            ...this.state.foto,
+            likeada: !this.state.foto.likeada
+        }
+        this.setState({ foto: fotoAtualizada });
+    }
+
     render() {
+        const { foto } = this.state;
+
         return (
             <View>
                 <View style={styles.cabecalho}>
                     <Image
-                        source={{ uri: this.props.foto.urlPerfil }}
+                        source={{ uri: foto.urlPerfil }}
                         style={styles.fotoDePerfil} />
-                    <Text>{this.props.foto.loginUsuario}</Text>
+                    <Text>{foto.loginUsuario}</Text>
                 </View>
                 <Image
-                    source={{ uri: this.props.foto.urlFoto }}
+                    source={{ uri: foto.urlFoto }}
                     style={styles.foto} />
+                <View style={styles.rodape} >
+                    <TouchableOpacity onPress={this.like.bind(this)}>
+                        <Image
+                            source={this.carregaIcone(foto.likeada)}
+                            style={styles.botaoDeLike} />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     };
@@ -43,5 +75,12 @@ const styles = StyleSheet.create({
     foto: {
         width: screenWidth,
         height: screenWidth
+    },
+    botaoDeLike: {
+        width: 40,
+        height: 40
+    },
+    rodape: {
+        margin: 10
     }
 });
